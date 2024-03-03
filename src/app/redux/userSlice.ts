2a057b3996b9faf35100175982e32008a1da0180
obaidmuneer <obaidmuneer55@gmail.com>
2024-03-03 16:55:56 +0500

    some changes

--- src/app/redux/userSlice.ts
+++ src/app/redux/userSlice.ts
@@ -6,6 +6,7 @@ export const fetchUserById = createAsyncThunk(
     async (userId: string) => {
         try {
             const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+            if (!response.ok) throw new Error(`fetch failed, status: ${response.status}`)
             return await response.json()
         } catch (error) {
             throw error
